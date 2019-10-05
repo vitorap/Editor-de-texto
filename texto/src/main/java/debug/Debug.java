f0b6f023dea9ad0348f1856c6d09aef7323a968b
vitorap <vitor.albuquerque12@gmail.com>
2019-10-05 18:34:58 -0300

    Primeira versao

---
+++ texto/src/main/java/debug/Debug.java
@@ -0,0 +1,12 @@
+package debug;
+
+/**
+ * Class that controls the verbosity of the program.
+ * @author computerjunky28
+ *
+ */
+public class Debug {
+	public static final boolean DEBUG = false;
+	public static final boolean VERBOSE = false; //general print statements 
+	//ie server started, client connected, etc.
+}
