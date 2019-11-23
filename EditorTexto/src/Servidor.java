743c7dd794b22bbdfb92196ff6d3f4495257d568
vitorap <vitor.albuquerque12@gmail.com>
2019-11-23 19:10:53 -0300

    Revert "t3 feito - em ingles"
    This reverts commit 72f411a5472230d69493df76a98c2a32de738165.

---
+++ EditorTexto/src/Servidor.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import java.util.ArrayList;
+/**
+ * Classe responsável por genrenciar os arquivos que estão sendo editados pelos clientes.
+ */
+
+/** Alunos:
+*@autor Vitor Albuquerque de Paula -8628220
+*@autor Caique Honorio Cardoso -8910222
+*/
+
+public class Servidor {
+   File f;
+
+    public File getF() {
+        return f;
+    }
+
+    /**
+     * 
+     * @param f 
+     */
+    public void setF(File f) {
+        this.f = f;
+    }
+
+        
+
+}
